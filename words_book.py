c095e008067fda41fe9018c445a2cd0b99e61c0c
makapolo123 <15572327801@163.com>
2026-07-27 18:56:32 +0800

    单词记忆辅助程序

---
+++ words_book.py
@@ -0,0 +1,159 @@
+import json
+import random
+
+SAVE_FILE = "words.json"
+MAX_PRO = 100
+MIN_PRO = 50
+ADD_POINT = 10
+SUB_POINT = 8
+
+words_data = []
+
+#保存单词
+def save_words():
+    """将单词列表保存到本地json文件"""
+    with open(SAVE_FILE, "w", encoding="utf-8") as f:
+        json.dump(words_data, f, ensure_ascii=False, indent=2)
+
+#加载单词
+def load_words():
+    """读取本地单词文件，无文件则初始化空单词列表"""
+    global words_data
+    try:
+        with open(SAVE_FILE, "r", encoding="utf-8") as f:
+            words_data = json.load(f)
+    except FileNotFoundError:
+        words_data = []
+
+#导入单词
+def import_txt(file_path):
+    """导入txt单词文件（格式：英文，中文\n）"""
+    global words_data
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            words = f.readlines()
+
+        for word in words:
+            word = word.strip()
+            if not word or "," not in word:  #跳过空行、没有逗号分隔的无效行
+                continue
+            #分割英文和中文
+            eng, chn = word.split(",", 1)
+            eng = eng.strip()
+            chn = chn.strip()
+            #判断单词是否已经存在
+            is_exist = False
+            for w in words_data:
+                if w["en"] == eng:
+                    is_exist = True
+                    break
+            if not is_exist:
+                new_word = {
+                    "en" : eng,
+                    "cn" : chn,
+                    "proficiency" : 50
+                }
+                words_data.append(new_word)
+
+        save_words()
+        print("单词导入成功")
+    except Exception as err:
+        print("导入失败，错误：", err)
+
+#抽取单词
+def pick_random_word():
+    """根据熟练度加权随机抽取单词，熟练度越低越容易出现"""
+    weight_list = []
+    for word in words_data:
+        #熟练度越低，权重越大
+        weight = 100 - word["proficiency"]
+        weight_list.append(weight)
+    #按权重抽取1个单词
+    target_word = random.choices(words_data, weights=weight_list, k=1)[0]
+    return target_word
+
+#判断所有单词是否达标
+def all_word_finish():
+    """判断所有单词熟练度是否都超过90"""
+    for word in words_data:
+        if word["proficiency"] < 90:
+            return False
+    return True
+
+#单词检测
+def start_review():
+    if len(words_data) == 0:
+        print("暂无单词，请先导入单词！")
+        return
+    print("\n========== 单词背诵模式 ==========")
+    print("操作指令：1=认识  2=不认识  0=退出背诵")
+    while True:
+        if all_word_finish():
+            print("所有单词已达标！")
+            break
+
+        #抽取单词
+        current_word = pick_random_word()
+        print(f"\n[单词]{current_word['en']}")
+        select = input("请输入你的选择：").strip()
+
+        #‘0’ 退出
+        if select == '0':
+            save_words()
+            print("进度已保存，退出")
+            break
+
+        #'1' 认识
+        elif select == '1':
+            print(f"释义：{current_word['cn']}")
+            next = input("操作指令：1=下一个  2=记错了  3=记住了：").strip()
+            while True:
+                #熟练度增加，最高不能超过MAX_PRO
+                if next == '1':
+                    current_word["proficiency"] = min(MAX_PRO, current_word["proficiency"] + ADD_POINT)
+                    break
+                #熟练度减少，最低不能小于0
+                elif next == '2':
+                    current_word["proficiency"] = max(MIN_PRO, current_word["proficiency"] - SUB_POINT)
+                    break
+                #已经记牢了，直接将熟练度改为达标值，后续不再出现
+                elif next == '3':
+                    current_word["proficiency"] = 90
+                    break
+                else:
+                    print("输入错误！请重新输入")
+
+        #'2' 不认识
+        elif select == '2':
+            print(f"释义：{current_word['cn']}")
+            # 熟练度减少，最低不能小于MIN_PRO
+            current_word["proficiency"] = max(MIN_PRO, current_word["proficiency"] - SUB_POINT)
+
+        else:
+            print("输入错误!请重新输入")
+            continue
+    save_words()
+
+#主菜单函数
+def main_menu():
+    load_words()
+    while True:
+        print("\n========== 单词记忆辅助程序 ==========")
+        print("1. 导入单词txt文件")
+        print("2. 开始背诵复习")
+        print("3. 退出程序")
+        choice = input("请选择功能序号：").strip()
+        if choice == "1":
+            file_path = input("请输入单词文件名称（例如 words.txt）：")
+            import_txt(file_path)
+        elif choice == "2":
+            start_review()
+        elif choice == "3":
+            save_words()
+            print("已保存所有进度，程序关闭！")
+            break
+        else:
+            print("输入无效，请重新选择！")
+
+if __name__ == "__main__":
+    main_menu()
