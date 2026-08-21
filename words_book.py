01e4c7c75147e2ee8c57b44ce1792ce4ee2e9a87
makapolo123 <15572327801@163.com>
2026-08-21 14:47:31 +0800

    新增记录复习的每个单词的情况

--- words_book.py
+++ words_book.py
@@ -1,26 +1,29 @@
-import datetime
+import csv
 import json
 import random
 import time
+from datetime import datetime
 
-SAVE_FILE = "words.json"
+WORDS_SAVE_FILE = "words.json"         # 单词本地存档文件
+STATISTIC_FILE = "statistic_data.txt"  # 复习数据文件
+RECORD_DATA_FILE = "word_record.csv"   # 复习的单词数据
 STD_SCORE = 90       # 达标熟练度
 MAX_PRO = 100        # 最大熟练度
 MIN_PRO = 50         # 最小熟练度
 ADD_POINT = 10       # 增加熟练度
 SUB_POINT = 8        # 减少熟练度
 
-words_data = []   # 单词列表
-total_words = 0   # 抽取的单词数
-new_words = 0     # 新单词数
+words_data = []    # 单词列表
+total_words = 0    # 抽取的单词数
+new_words = 0      # 新单词数
 remember_words = 0 # 记住的单词数
-start_time = 0
-end_time = 0
+start_time = 0     # 复习开始时间
+end_time = 0       # 复习结束时间
 
 # 保存单词
 def save_words():
     """将单词列表保存到本地json文件"""
-    with open(SAVE_FILE, "w", encoding="utf-8") as f:
+    with open(WORDS_SAVE_FILE, "w", encoding="utf-8") as f:
         json.dump(words_data, f, ensure_ascii=False, indent=2)
 
 # 加载单词
@@ -28,7 +31,7 @@ def load_words():
     """读取本地单词文件，无文件则初始化空单词列表"""
     global words_data
     try:
-        with open(SAVE_FILE, "r", encoding="utf-8") as f:
+        with open(WORDS_SAVE_FILE, "r", encoding="utf-8") as f:
             words_data = json.load(f)
     except FileNotFoundError:
         words_data = []
@@ -80,9 +83,9 @@ def forgetting_reduce():
         last = word.get("last_review", 0)
         if last == 0 or word["proficiency"] == 101:
             continue
-        reduce = max(0, (now - last) // 86400 * 2 - 1)     # 随着时间间隔变长而减少相应的熟练度
+        reduce = int(max(0, (now - last) // 86400 * 2 - 1))     # 随着时间间隔变长而减少相应的熟练度
         if reduce > word["last_reduce"]:
-            word["proficiency"] = max(MIN_PRO, word["proficiency"] - reduce)
+            word["proficiency"] = int(max(MIN_PRO, word["proficiency"] - reduce))
             word["last_reduce"] = reduce
         
 # 抽取单词
@@ -118,11 +121,45 @@ def all_word_finish():
             return False
     return True
 
+# 记录每次复习的数据
+def statistic_data():
+    global review_time
+    with open(STATISTIC_FILE, "a", encoding="utf-8") as f:
+        f.write(f"复习时间：{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n")
+        review_time = int(end_time - start_time)
+        f.write(f"用时：{int(review_time // 3600)}:{int(review_time % 3600 // 60)}:{int(review_time % 60)}\n")
+        f.write(f"复习单词总数：{total_words}\n")
+        f.write(f"新单词数：{new_words}\n")
+        f.write(f"记住单词数：{remember_words}\n")
+        f.write(f"正确率：{round(remember_words / total_words, 4)*100}%\n")
+        f.write("\n")
+
+# 复习的每个单词的情况
+def word_record(word_record_data):
+    with open(RECORD_DATA_FILE, "a+", newline="", encoding="utf-8") as f:
+        f.seek(0)
+        reader = csv.reader(f)
+        # 第一行为表头
+        first_row = next(reader, None)
+        writer = csv.writer(f)
+        if first_row is None:
+            writer.writerow(["复习时间", "英文", "中文", "初始熟练度", "最终熟练度", "结果"])
+        writer.writerows([word_record_data])
+
+
 # 单词检测
 def start_review():
+    global total_words
+    global remember_words
+    global start_time
+    global end_time
+
     if len(words_data) == 0:
         print("暂无单词，请先导入单词！")
         return
+    
+    start_time = time.time()
+    word_record_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
     print("\n========== 单词背诵模式 ==========")
     print("操作指令：1=认识  2=不认识  0=退出背诵")
@@ -136,15 +173,17 @@ def start_review():
         # 抽取单词
         current_word = pick_random_word()
 
-        total_words += 1
         if current_word["last_review"] == 0:
             new_words += 1
+        initial_proficiency = current_word["proficiency"]
 
         print(f"\n[单词]{current_word['en']}")
         select = input("请输入你的选择：").strip()
-
+        
         # ‘0’ 退出
         if select == '0':
+            end_time = time.time()
+            statistic_data()
             save_words()
             print("进度已保存，退出")
             break
@@ -156,16 +195,19 @@ def start_review():
             while True:
                 # 熟练度增加，最高不能超过MAX_PRO
                 if next_act == '1':
-                    current_word["proficiency"] = min(MAX_PRO, current_word["proficiency"] + ADD_POINT)
+                    current_word["proficiency"] = int(min(MAX_PRO, current_word["proficiency"] + ADD_POINT))
                     remember_words += 1
+                    result = "remembered"
                     break
                 # 熟练度减少，最低不能小于MIN_PRO
                 elif next_act == '2':
-                    current_word["proficiency"] = max(MIN_PRO, current_word["proficiency"] - SUB_POINT)
+                    current_word["proficiency"] = int(max(MIN_PRO, current_word["proficiency"] - SUB_POINT))
+                    result = "forgotten"
                     break
                 # 已经记牢了，将熟练度改为特殊值，后续不再出现
                 elif next_act == '3':
                     current_word["proficiency"] = 101
+                    result = "marked_remembered"
                     break
                 else:
                     print("输入错误！请重新输入！")
@@ -175,24 +217,20 @@ def start_review():
         elif select == '2':
             print(f"释义：{current_word['cn']}")
             # 熟练度减少，最低不能小于MIN_PRO
-            current_word["proficiency"] = max(MIN_PRO, current_word["proficiency"] - SUB_POINT)
+            current_word["proficiency"] = int(max(MIN_PRO, current_word["proficiency"] - SUB_POINT))
+            result = "forgotten"
 
         else:
             print("输入错误!请重新输入！")
             continue
-        # 记录复习时间
+        # 记录复习情况
+        total_words += 1
         current_word["last_review"] = time.time()
+        final_proficiency = current_word["proficiency"]
+        word_record([word_record_time, current_word["en"], current_word["cn"], initial_proficiency, final_proficiency, result])
         # 减少的熟练度归零
         current_word["reduce"] = 0
         save_words()
-
-def statistic_data():
-    with open("statistic_data.txt", "a", encoding="utf-8") as f:
-        f.write(f"复习时间：{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}")
-        review_time = start_time - end_time
-        f.write(f"用时{int(review_time // 3600)}:{int(review_time % 3600 // 60)}:{review_time % 60}")
-        f.write("复习单词总数\t新单词数\t记住单词数\t正确率")
-        f.write(f"{total_words}")
 
 # 主菜单函数
 def main_menu():
@@ -207,10 +245,8 @@ def main_menu():
             file_path = input("请输入单词文件名称（例如 words.txt）：")
             import_txt(file_path)
         elif choice == "2":
-            start_time = time.time()
             start_review()
-        elif choice == "3":
-            end_time = time.time()
+        elif choice == "3":        
             save_words()
             print("已保存所有进度，程序关闭！")
             break
